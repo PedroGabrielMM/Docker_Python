ca182112e2cfd0e58f320be9c232db1117d787de
PedroGabrielMM <gabrielmacedo1225@gmail.com>
2025-12-07 18:24:40 -0300

    first commit

---
+++ install.ps1
@@ -0,0 +1,8 @@
+# Cria o ambiente virtual
+python -m venv .venv
+
+# Ativa o ambiente virtual no PowerShell
+. ..venv\Scripts\Activate.ps1
+
+# Instala as dependências
+pip install -r requirements.txt
