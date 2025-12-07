9922e5672b76bae1fe51b927e8df216376acfa89
PedroGabrielMM <gabrielmacedo1225@gmail.com>
2025-12-07 18:30:02 -0300

    corrigindo install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 python -m venv .venv
 
 # Ativa o ambiente virtual no PowerShell
-. ..venv\Scripts\Activate.ps1
+. .\.venv\Scripts\Activate.ps1
 
 # Instala as dependências
 pip install -r requirements.txt
