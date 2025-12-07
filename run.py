d445a94282c6000cfa09613274f386759385cdad
PedroGabrielMM <gabrielmacedo1225@gmail.com>
2025-12-07 19:28:33 -0300

    implementando primeiro route

--- run.py
+++ run.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Olá, estou na aplicação setada!'
+
+# Para rodar a aplicação é necessário rodar o comando abaixo:
+# flask --app run.py run
